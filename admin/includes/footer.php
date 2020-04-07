f565265f678e7014d2bc5d53fa71f15707e2d8e2
eyradel <60956082+eyradel@users.noreply.github.com>
2020-04-07 01:21:31 -0700

    my first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong> &copy; 2020 Brought To You By <a href="www.napsternetwork.wordpress.com">Napster</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2020 Brought To You By<a href="www.napsternetwork.wordpress.com"> Napsters</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
